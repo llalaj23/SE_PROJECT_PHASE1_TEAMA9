826959e28556f4bf5ec81b3f0f291f87a1eb8253
AndreaShtjefni <ashtjefni23@epoka.edu.al>
2026-05-10 19:25:32 +0200

    Delete views.py

--- views.py
+++
@@ -1,8 +0,0 @@
-from django.contrib.auth.decorators import login_required
-from django.shortcuts import render
-
-
-@login_required
-def profile(request):
-    """Shows the logged-in user's own profile page."""
-    return render(request, 'users/profile.html', {'user': request.user})
